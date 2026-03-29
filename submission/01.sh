46ba5a726c190a480315202457682b0008f2fd52
UfiairENE <irenecufia@gmail.com>
2026-03-29 10:43:34 +0300

    feat: complete chapter 6 and 7 exercises — multisig and PSBT transactions

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,5 @@
 # Create a wallet with the name "builderswallet".
+
+#!/bin/bash
+
+bitcoin-cli -regtest -named createwallet wallet_name="builderswallet"
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,10 @@
 # Create a native segwit address and get the public key from the address.
+
+#!/bin/bash
+
+
+address=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getnewaddress)
+
+pubkey=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getaddressinfo address="$address" | jq -r '.pubkey')
+
+echo $pubkey
